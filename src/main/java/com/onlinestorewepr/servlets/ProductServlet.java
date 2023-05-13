864da1ae5273b9388c5359858061b50cd84b40aa
nguyenkieuchauanh0908 <92566019+nguyenkieuchauanh0908@users.noreply.github.com>
2023-05-13 19:15:47 +0700

    ChauAnh_demo_searchProducts_seeProDetail

--- src/main/java/com/onlinestorewepr/servlets/ProductServlet.java
+++ src/main/java/com/onlinestorewepr/servlets/ProductServlet.java
@@ -1,19 +1,25 @@
 package com.onlinestorewepr.servlets;
 
+import com.onlinestorewepr.models.Product;
 import com.onlinestorewepr.services.ProductService;
+import com.onlinestorewepr.utils.Validator;
 
+import javax.naming.directory.SearchResult;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(value = {"/admin/products"})
 public class ProductServlet extends HttpServlet {
+
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     ProductService productService = new ProductService(request, response);
     productService.listProducts();
   }
+
 }
