5ea7533c54036f62d6bfeb77fe9136decf31b62e
dihuynh@outlook.sg <dihuynh@outlook.sg>
2023-05-14 00:34:57 +0700

    di modified

--- src/main/java/com/onlinestorewepr/services/UserService.java
+++ src/main/java/com/onlinestorewepr/services/UserService.java
@@ -2,8 +2,16 @@ package com.onlinestorewepr.services;
 
 public class UserService {
     public void sendOTP() {}
-    public void sendEmail() {}
-    public void changePassword() {}
-    public void forwardToSettingPass() {}
     public void validateOTP() {}
+    public void changePassForgot() {}
+    public void getInput(){}
+    public void forwardToSendOTPPage() {}
+    public void sendOTPtoEmail(String inputEmail) {}
+    public void forwardToEnterOTPPage(){}
+    public void forwardToSettingPass() {}
+    public void forwardToChangePassForgotPage(){}
+    public void forwardToLoginPage(){}
+    public void forwardToEditUserPage(){}
+    public void forwardToProfilePage(){}
+
 }
